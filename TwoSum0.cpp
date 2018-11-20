293f2147ce1f43cf2a96eeba491d30e1685ea0d4
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-11-20 16:25:18 +0800

    Brute Force
    The brute force approach is simple. Loop through each element x and find if there is another value that equals to target - x.

--- TwoSum0.cpp
+++ TwoSum0.cpp
@@ -13,11 +13,11 @@ public:
     }
 
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices{0, 0};
+        vector<int> indices{0, 0};  //初始化vector对象
         int j(0);
         
         for(int i = 0; i < nums.size(); i++){
-            indices[0] = i;
+            indices[0] = i; //下标运算符访问vector对象的元素
             
             j = findSecondIndex(nums, target - nums[i], i+1);
             indices[1] = j;
