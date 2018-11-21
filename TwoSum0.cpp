2b3a1708e055b9256b687cec6c2288f18ce0dd30
AliceGuo9401 <45186543+AliceGuo9401@users.noreply.github.com>
2018-11-21 08:24:09 +0800

    Brute Force
    The brute force approach is simple. Loop through each element x and find if there is another value that equals to target - x.

--- TwoSum0.cpp
+++ TwoSum0.cpp
@@ -1,29 +1,61 @@
-class Solution {
-public:
-    int findSecondIndex(vector<int>& nums, int target, int start) {
-        int secondIndex = 0;
-        int i(start);
-        
-        while(i < nums.size()) {
-            if(nums[i] == target)
-                return secondIndex = i;
-            i++;
-            }
-        return 0;
-    }
+#include <vector>
+#include <iostream>
 
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices{0, 0};  //初始化vector对象
-        int j(0);
-        
-        for(int i = 0; i < nums.size(); i++){
-            indices[0] = i; //下标运算符访问vector对象的元素
-            
-            j = findSecondIndex(nums, target - nums[i], i+1);
-            indices[1] = j;
-            if(indices[1] != 0 && i < indices[1] && indices[1] < nums.size())
-                break;
+using namespace std;
+/*start*/
+int findSecondIndex(vector<int>& nums, int target, int start) {
+    //int low = start, high = nums.size() - 1;
+    int secondIndex = 0;
+    /*if(nums[0] < nums[1]) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < target)
+                low = mid + 1;
+            else if (target < nums[mid])
+                high = mid - 1;
+            else
+                return secondIndex = mid;
         }
-        return indices;
     }
-};
+    else if(nums[0] > nums[1]) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target)
+                low = mid + 1;
+            else if (target < nums[mid])
+                high = mid - 1;
+            else
+                return secondIndex = mid;
+        }
+    }*/
+    //else{
+    int i(start);
+    while(i < nums.size()) {
+        if(nums[i] == target)
+            return secondIndex = i;
+        i++;
+    }
+    //}
+    return 0;
+}
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> indices{0, 0};  //初始化vector对象
+    int j(0);
+    for(int i = 0; i < nums.size(); i++){
+        indices[0] = i; //使用下标运算符访问vector对象
+        j = findSecondIndex(nums, target - nums[i], i+1);
+        indices[1] = j;
+        if(indices[1] != 0 && i < indices[1] && indices[1] < nums.size())
+            break;
+    }
+    return indices;
+}
+/*end*/
+int main() {
+    vector<int> givenNums{3, 2, 4};
+    int givenTarget = 6;
+    vector<int> coutNums = twoSum(givenNums, givenTarget);
+    cout << coutNums[0] << "," << coutNums[1] << endl;
+    return 0;
+}
